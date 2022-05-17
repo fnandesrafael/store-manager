dafec36e7a3abbeff1ac9becc4286a31e14c8a7d
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-05-17 17:33:58 -0300

    feat: adiciona express.json no body da aplicação

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Router = require('./routes/index');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
