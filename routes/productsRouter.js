933a963a9f233395d570acf64499ab0df921be7b
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-05-17 17:36:37 -0300

    feat: adiciona rotas de post com o middleware de validação aos routers de produtos e vendas

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
+const validationMiddlewares = require('../middlewares/validationMiddlewares');
 
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
 
 router.get('/:id', productsController.getProductById);
+
+router.post('/', validationMiddlewares.productValidation, productsController.postProduct);
 
 module.exports = router;
--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const salesController = require('../controllers/salesController');
+const validationMiddlewares = require('../middlewares/validationMiddlewares');
 
 const router = express.Router();
 
 router.get('/', salesController.getSales);
 
 router.get('/:id', salesController.getSaleById);
+
+router.post('/', validationMiddlewares.saleValidation, salesController.postSale);
 
 module.exports = router;
