00a3acccfd924e21eb92482e9c7739ad429b4f09
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-24 15:26:59 -0300

    remoção de alguns arquivos do stage do git

--- src/tests/unit/models/Product.test.js
+++ src/tests/unit/models/Product.test.js
@@ -6,15 +6,15 @@ const { newProductMock } = require('../../mocks/Product');
 const Product = require('../../../database/models/Product')
 
 describe('Testa a model Product', () => {
-  before(() => {
-    sinon.stub(connection, 'query').resolves([{ insertId: 0 }, undefined])
-  })
-
-  after(() => {
-    sinon.restore();
-  })
-
   describe('quando é criado um novo produto com sucesso', () => {
+    before(() => {
+      sinon.stub(connection, 'query').resolves([{ insertId: 0 }, undefined])
+    })
+  
+    after(() => {
+      sinon.restore();
+    })
+    
     it('é retornado um objeto', async () => {
       const sut = await Product.createProduct(newProductMock)
 
