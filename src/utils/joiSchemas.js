293f8207c0476cca5aa0f5295c3717598089ac43
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-24 18:00:58 -0300

    fix: adição de mensagem de rro do joi para tipos diferentes

--- src/utils/joiSchemas.js
+++ src/utils/joiSchemas.js
@@ -7,13 +7,15 @@ const productSchema = Joi.object({
     .messages({
       'any.required': '400,"name" is required',
       'string.min': '422,"name" length must be at least 5 characters long',
+      'string.base': '400,"name" must be a string',
     }),
   quantity: Joi.number()
-    .min(1)
     .required()
+    .min(1)
     .messages({
       'any.required': '400,"quantity" is required',
       'number.min': '422,"quantity" must be greater than or equal to 1',
+      'number.base': '400,"quantity" must be a number',
     }),
 });
 
