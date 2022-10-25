0823340e2821303451de32fc9b4acc4ea4655004
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-25 08:49:21 -0300

    feat: implementação dos testes da camada productService no método getProducts

--- src/tests/unit/services/productService.test.js
+++ src/tests/unit/services/productService.test.js
@@ -3,7 +3,7 @@ const { describe } = require('mocha');
 const sinon = require('sinon');
 const Product = require('../../../database/models/Product');
 const productService = require('../../../services/productService');
-const { newProductMock, newProductPayload } = require('../../mocks/Product');
+const { newProductMock, newProductPayload, allProductsMock } = require('../../mocks/Product');
 
 describe('02 - Testa a service productService', () => {
   describe('quando é criado um novo produto', () => {
@@ -46,6 +46,48 @@ describe('02 - Testa a service productService', () => {
         const sut = await productService.createProduct(newProductPayload)
 
         expect(sut).to.have.all.keys('id', 'name', 'quantity')
+      });
+    });
+  });
+
+  describe('quando são buscados todos os produtos com sucesso', () => {
+    describe('e existem cadastros no banco de dados', () => {
+      before(() => {
+        sinon.stub(Product, 'getProducts').resolves(allProductsMock)
+      });
+  
+      after(() => {
+        sinon.restore();
+      });
+
+      it('é retornado um array de objetos', async () => {
+        const sut = await productService.getProducts()
+
+        expect(sut).to.be.an('array')
+        expect(sut[0]).to.be.an('object')
+      });
+  
+      it('os objetos contém as chaves: "id", "name" e "quantity"', async () => {
+        const sut = await productService.getProducts()
+
+        expect(sut[0]).to.have.all.keys('id', 'name', 'quantity')
+      });
+    });
+
+    describe('e não existem cadastros no banco de dados', () => {
+      before(() => {
+        sinon.stub(Product, 'getProducts').resolves([])
+      });
+  
+      after(() => {
+        sinon.restore();
+      });
+
+      it('é retornado um array vazio', async () => {
+        const sut = await productService.getProducts()
+
+        expect(sut).to.be.an('array')
+        expect(sut).to.be.empty
       });
     });
   });
