5a096b5c1b549c8214bee6b63e464c0f27f6f49d
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-26 15:50:07 -0300

    feat: implementação de testes para a camada productController no método getProductById

--- src/tests/unit/controllers/productController.test.js
+++ src/tests/unit/controllers/productController.test.js
@@ -3,7 +3,7 @@ const { describe } = require('mocha');
 const sinon = require('sinon');
 const productController = require('../../../controllers/productController');
 const productService = require('../../../services/productService');
-const { newProductMock, newProductPayload, allProductsMock } = require('../../mocks/Product');
+const { newProductMock, newProductPayload, allProductsMock, searchedProductMock } = require('../../mocks/Product');
 
 const req = {}
 const res = {}
@@ -36,7 +36,7 @@ describe('Testa o controller productController', () => {
     });
   });
 
-  describe('quando é criado um novo produto', () => {
+  describe('quando são buscados todos os produtos', () => {
     before(() => {
       sinon.stub(productService, 'getProducts').resolves(allProductsMock);
 
@@ -48,18 +48,43 @@ describe('Testa o controller productController', () => {
       sinon.restore();
     });
     
-    it('o método status é chamado com o valor 201', async () => {
-      req.body = newProductPayload;
+    it('o método status é chamado com o valor 200', async () => {
       await productController.getProducts(req, res);
 
       expect((res.status).calledWith(200)).to.be.true;
     });
 
-    it('o método json é chamado com o objeto criado', async () => {
-      req.body = newProductPayload;
+    it('o método json é chamado com todos os produtos cadastrados', async () => {
       await productController.getProducts(req, res);
 
       expect((res.json).calledWith(allProductsMock)).to.be.true;
+    });
+  });
+
+  describe('quando é buscado um produto em específico', () => {
+    before(() => {
+      sinon.stub(productService, 'getProductById').resolves(searchedProductMock);
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+    
+    it('o método status é chamado com o valor 200', async () => {
+      req.params = { id: 1 };
+      await productController.getProductById(req, res);
+
+      expect((res.status).calledWith(200)).to.be.true;
+    });
+
+    it('o método json é chamado com o produto pesquisado', async () => {
+      req.params = { id: 1 };
+      await productController.getProductById(req, res);
+
+      expect((res.json).calledWith(searchedProductMock)).to.be.true;
     });
   });
 });
