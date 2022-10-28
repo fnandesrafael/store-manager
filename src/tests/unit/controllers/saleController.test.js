3e095bd921af117c5f61ab49ebb389561c7e9d0e
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-28 09:41:10 -0300

    feat: implementação dos testes da camada saleController na função getSaleById

--- src/tests/unit/controllers/saleController.test.js
+++ src/tests/unit/controllers/saleController.test.js
@@ -3,7 +3,7 @@ const { describe } = require('mocha');
 const sinon = require('sinon');
 const saleController = require("../../../controllers/saleController");
 const saleService = require('../../../services/saleService');
-const { newSaleMock, newSalePayload, allSalesMock } = require('../../mocks/Sale');
+const { newSaleMock, newSalePayload, allSalesMock, searchedSaleMock } = require('../../mocks/Sale');
 
 const req = {}
 const res = {}
@@ -58,6 +58,33 @@ describe('Testa o controller saleController', () => {
       await saleController.getSales(req, res);
 
       expect((res.json).calledWith(allSalesMock)).to.be.true;
+    });
+  });
+
+  describe('quando é pesquisada uma venda específica', () => {
+    before(() => {
+      sinon.stub(saleService, 'getSaleById').resolves(searchedSaleMock);
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+    
+    it('o método status é chamado com o valor 200', async () => {
+      req.params = { id: 1 }
+      await saleController.getSaleById(req, res);
+
+      expect((res.status).calledWith(200)).to.be.true;
+    });
+
+    it('o método json é chamado com o objeto criado', async () => {
+      req.params = { id: 1 }
+      await saleController.getSaleById(req, res);
+
+      expect((res.json).calledWith(searchedSaleMock)).to.be.true;
     });
   });
 });
