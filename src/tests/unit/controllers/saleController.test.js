6d5105dfd6283d047f83ca54a674e5e223dc8c2e
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-28 09:38:56 -0300

    feat: implementação dos testes da camada saleController nas funções createSale e getSales

---
+++ src/tests/unit/controllers/saleController.test.js
@@ -0,0 +1,63 @@
+const { expect } = require('chai');
+const { describe } = require('mocha');
+const sinon = require('sinon');
+const saleController = require("../../../controllers/saleController");
+const saleService = require('../../../services/saleService');
+const { newSaleMock, newSalePayload, allSalesMock } = require('../../mocks/Sale');
+
+const req = {}
+const res = {}
+
+describe('Testa o controller saleController', () => {
+  describe('quando é criada uma nova venda', () => {
+    before(() => {
+      sinon.stub(saleService, 'createSale').resolves(newSaleMock);
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+    
+    it('o método status é chamado com o valor 201', async () => {
+      req.body = newSalePayload;
+      await saleController.createSale(req, res);
+
+      expect((res.status).calledWith(201)).to.be.true;
+    });
+
+    it('o método json é chamado com o objeto criado', async () => {
+      req.body = newSalePayload;
+      await saleController.createSale(req, res);
+
+      expect((res.json).calledWith(newSaleMock)).to.be.true;
+    });
+  });
+
+  describe('quando são pesquisadas todas as vendas', () => {
+    before(() => {
+      sinon.stub(saleService, 'getSales').resolves(allSalesMock);
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+    
+    it('o método status é chamado com o valor 200', async () => {
+      await saleController.getSales(req, res);
+
+      expect((res.status).calledWith(200)).to.be.true;
+    });
+
+    it('o método json é chamado com o objeto criado', async () => {
+      await saleController.getSales(req, res);
+
+      expect((res.json).calledWith(allSalesMock)).to.be.true;
+    });
+  });
+});
