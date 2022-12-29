5833527612dc8a1c43351ce37071286c32abffbf
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-12-29 12:15:23 -0300

    fix: remoção de porta no create pool da connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,7 +6,6 @@ const connection = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
 });
 
 module.exports = connection;
