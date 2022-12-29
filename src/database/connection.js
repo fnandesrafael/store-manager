c5f344de571101abedf9728bb806c20a098df396
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-12-29 12:20:46 -0300

    fix: nova tentativa de correção de porta para deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,6 +6,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.PORT,
 });
 
 module.exports = connection;
